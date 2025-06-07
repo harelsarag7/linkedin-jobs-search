d386d168745c902a661e523a4c64ec534421a4f4
Harel Sarag <144249148+Harel-Sarag@users.noreply.github.com>
2025-06-07 14:32:21 +0300

    Update recentJobs.ts

--- src/services/recentJobs.ts
+++ src/services/recentJobs.ts
@@ -63,6 +63,7 @@ export const fetchLinkedInJobs = async (cookie: string, keyword: string, locatio
     for (const job of jobs) {
         job.description = await getJobDescription(job.url);
         if(job.description && resumeText) {
+            console.log(`Detecting match score for job: ${job.title}`);
             job.matchScore = await detectMatchScore(job.description, resumeText)
         }
     }
